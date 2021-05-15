fe698bce168b4423e46d87e91b3f04a92407015a
Argen <n.uulu.a@mail.ru>
2021-05-15 14:27:51 +0900

    UIButton, Label, TextField

--- Practice 1/ViewController.swift
+++ Practice 1/ViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var lbl: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
+    
+    @IBAction func changeText(_ sender: UITextField) {
+        let label = "Hello, "
+        let text = sender.text?.capitalized
+        lbl.text = label + text!
+    }
+    
+    @IBAction func changePrint(_ sender: UISlider) {
+        //Izmenit' razmer shrifta
+        let size = CGFloat(sender.value)
+//        lbl.font = lbl.font.withSize(size)
+        lbl.font = UIFont(name: "", size: size)
+    }
+    
+    @IBAction func preeButton(_ sender: UIButton) {
+        //Po najatiyu knopka vyvedet privetstvie
+        let label = "Hello, "
+        let text = textField.text?.capitalized
+        lbl.text = label + text!
+    }
 
 }
 
