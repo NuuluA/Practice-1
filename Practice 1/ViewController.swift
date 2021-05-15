228b4d6e2a1711b4faa3e3950e19c83d67fa5344
Argen <n.uulu.a@mail.ru>
2021-05-15 13:02:20 +0900

    Initial Commit

---
+++ Practice 1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Practice 1
+//
+//  Created by Арген on 15.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
